2816642aeb6891ea2b668ff4253eed4bf09e12b7
TeaChris <spiritsofafrica.nft@gmail.com>
2022-08-04 16:23:53 +0100

    nav scroll and toggle, responsiveness

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+// NAV SCROLL BACKGROUND
+
 $(function() {
     $(document).scroll(function(){
         var $nav = $('.nav');
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     })
 })
+
+
+// NAV TOGGLE
+
+var menu = document.getElementById("bar")
+var items = document.getElementById("navbar");
+
+items.style.right = "-360px";
+
+menu.onclick = function() {
+    if (items.style.right == "-360px") {
+        items.style.right = "0"; 
+    }
+    else {
+        items.style.right = "-360px";
+    }
+}
